ad1124bfee45d2092d2f3cfe5f193d65eb527a24
Sean Ferguson <sean@thefergies.net>
2017-06-10 15:08:18 -0700

    Initial checkin

---
+++ climateControlOn/index.js
@@ -0,0 +1,29 @@
+"use strict";
+var carwings = require("carwings2");
+
+module.exports = function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+
+    if (req.body.username && req.body.password) {
+        var client = new carwings.Client();
+        client.login(req.body.username, req.body.password, (err, vehicle) => {
+            if (err) {
+                context.log(err);
+            }
+            else {
+                context.log("Requesting HVAC on");
+
+                client.requestHvacOn(vehicle.vin, (statusErr, statusResponse) => {
+                    if (statusErr) {
+                        context.log(statusErr);
+                    }
+                    else {
+                        context.log(statusResponse);
+                    }
+                });
+            }
+        });
+    }
+    
+    context.done(null, {});
+};
