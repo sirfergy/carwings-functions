70cdb72398d4be04496613f4c0c0744f36e5c671
Sean Ferguson <sean@thefergies.net>
2018-04-08 22:13:41 -0700

    switch to non-async for azure

--- unlockDoors/index.js
+++ unlockDoors/index.js
@@ -2,24 +2,25 @@
 var carwings = require("carwings3");
 var https = require("https");
 
-module.exports = async function (context, req) {
+module.exports = function (context, req) {
     context.log('Unlock request');
 
     if (req.body.vin && req.body.username && req.body.password && req.body.authorizationKey) {
-        const { vin, username, password, authorizationKey } = req.body;
-        const client = new carwings.Client(vin);
-        try {
-            await client.login(username, password);
-            await client.unlockDoors(authorizationKey);
-            if (process.env["ifttt_unlock_url"]) {
-                https.get(process.env["ifttt_unlock_url"]);
-            }
-        } catch (ex) {
-            context.log(ex);
+        const errorHandler = (error) => {
+            context.log(error);
             if (process.env["ifttt_unlockerr_url"]) {
                 https.get(process.env["ifttt_unlockerr_url"]);
             }
-        }
+        };
+        const { vin, username, password, authorizationKey } = req.body;
+        const client = new carwings.Client(vin);
+        client.login(username, password).then(() => {
+            return client.unlockDoors(authorizationKey);
+        }, errorHandler).then(() => {
+            if (process.env["ifttt_unlock_url"]) {
+                https.get(process.env["ifttt_unlock_url"]);
+            }
+        }, errorHandler);
     }
 
     context.done(null, {});
